ad4576b5c78349a5bc98be0a8fd0deb00b598ceb
Initzzh <2212643256@qq.com>
2020-08-26 10:17:46 +0800

    do th. in other.test() and test.main()

--- src/test/Other.java
+++ src/test/Other.java
@@ -1,5 +1,7 @@
 package test;
 
 public class Other {
-
+	public void test(){
+		System.out.println("hello git");
+	}
 }
--- src/test/test.java
+++ src/test/test.java
@@ -3,5 +3,8 @@ package test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		Other other1=new Other();
+		other1.test();
+		
 	}
 }
